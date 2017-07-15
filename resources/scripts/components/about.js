78026aed41f2451a3793a7674b7d9274582e0603
Masato Seki <masatoseki@the-neighbourhood.com>
2017-07-15 13:10:14 +0100

    Implemented about section

--- resources/scripts/components/about.js
+++ resources/scripts/components/about.js
@@ -2,41 +2,22 @@ import $ from 'jquery';
 import ee from './ee';
 
 const $aboutCopyOpener = $('.js-about-copy-opener');
+const aboutHeight = $('.js-about-copy').height();
+const $aboutBody = $('.js-about-body');
 
 function openMoreCopy() {
-  console.log('open');
-  const aboutHeight = $('.js-about-copy').height();
-  $('.js-about-body').addClass('active').height(aboutHeight);
-  // if ($(this).closest('.js-accordion-nav-item').hasClass('active')) {
-  //   $(this).closest('.js-accordion-nav-item').removeClass('active');
-  // } else {
-  //   $(this).closest('.js-accordion-nav-item').addClass('active');
-  // }
+  $aboutBody.addClass('active').height(aboutHeight);
+}
+
+function addReadMore() {
+  if (aboutHeight < 180) {
+    $aboutBody.addClass('active').height(aboutHeight);
+  }
 }
 
 function init() {
+  addReadMore();
   $aboutCopyOpener.click(openMoreCopy);
 }
 
 ee.addListener('init', init);
-
-
-// function isOpen() {
-//   return $body.hasClass(activeFlagName);
-// }
-
-// function openNav() {
-//   $body.addClass(activeFlagName);
-// }
-
-// function closeNav() {
-//   $body.removeClass(activeFlagName);
-// }
-
-// function toggleNav() {
-//   if (!isOpen()) {
-//     openNav();
-//   } else {
-//     closeNav();
-//   }
-// }
