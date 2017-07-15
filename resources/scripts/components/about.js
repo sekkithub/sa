5022caa490cb6f3d4b7216d665e14c986d9f9b1c
Masato Seki <masatoseki@the-neighbourhood.com>
2017-07-15 14:30:19 +0100

    WIP about bug fix

--- resources/scripts/components/about.js
+++ resources/scripts/components/about.js
@@ -2,14 +2,15 @@ import $ from 'jquery';
 import ee from './ee';
 
 const $aboutCopyOpener = $('.js-about-copy-opener');
-const aboutHeight = $('.js-about-copy').height();
 const $aboutBody = $('.js-about-body');
 
 function openMoreCopy() {
+  const aboutHeight = $('.js-about-copy').height();
   $aboutBody.addClass('active').height(aboutHeight);
 }
 
 function addReadMore() {
+  const aboutHeight = $('.js-about-copy').height();
   if (aboutHeight < 180) {
     $aboutBody.addClass('active').height(aboutHeight);
   }
