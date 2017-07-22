c8207adcb6bf1c025c822c974213c091f110de5d
Masato Seki <masatoseki@the-neighbourhood.com>
2017-07-22 15:45:38 +0100

    Carousel style

--- resources/scripts/components/accordion-nav.js
+++ resources/scripts/components/accordion-nav.js
@@ -2,10 +2,15 @@ import $ from 'jquery';
 import ee from './ee';
 
 function openNav() {
-  if ($(this).closest('.js-accordion-nav-item').hasClass('active')) {
-    $(this).closest('.js-accordion-nav-item').removeClass('active');
+  const $selectedItem = $(this).closest('.js-accordion-nav-item');
+
+  if ($selectedItem.hasClass('active')) {
+    $selectedItem.removeClass('active');
+    // $selectedItem.find('.js-carousel').css('min-height', 0);
   } else {
-    $(this).closest('.js-accordion-nav-item').addClass('active');
+    $selectedItem.addClass('active');
+    // const activeCarouselHeight = $selectedItem.find('.slick-active').height();
+    // $selectedItem.find('.js-carousel').css('min-height', activeCarouselHeight);
   }
 }
 
