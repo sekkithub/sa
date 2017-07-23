f169085bc711e5dac7800bb04d429b20ceafe7a7
Masato Seki <masatoseki@the-neighbourhood.com>
2017-07-23 23:58:01 +0100

    WIP carousel height bug fix

--- resources/scripts/components/accordion-nav.js
+++ resources/scripts/components/accordion-nav.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
 import ee from './ee';
 
+// function setHeight() {
+//   if ($selectedItem.hasClass('active')) {
+//     const activeCarouselHeight = $selectedItem.find('.slick-active').height();
+//     $selectedItem.find('.js-accordion-nav-content').css('height', activeCarouselHeight);
+//   }
+// }
+
 function openNav() {
   const $selectedItem = $(this).closest('.js-accordion-nav-item');
-
   if ($selectedItem.hasClass('active')) {
     $selectedItem.removeClass('active');
     $selectedItem.find('.js-accordion-nav-content').css('height', 0);
@@ -16,6 +22,9 @@ function openNav() {
 
 function init() {
   $(document).off().on('click', '.js-accordion-nav-opener', openNav);
+  // setHeight();
+  // $(window).resize(setHeight);
+  // $(window).on('orientationchange', setHeight);
 }
 
 ee.addListener('init', init);
--- resources/scripts/components/carousels.js
+++ resources/scripts/components/carousels.js
@@ -2,13 +2,14 @@ import $ from 'jquery';
 import 'slick-carousel';
 import ee from './ee';
 
-function carouselsInit() {
-  const $carousels = $('.js-carousel');
+const $carousels = $('.js-carousel');
 
+function carouselsInit() {
   $carousels.each((i, el) => {
     $(el).not('.slick-initialized').slick({
       dots: false,
       fade: true,
+      // adaptiveHeight: true,
       slidesToShow: 1,
       lazyLoad: 'progressive',
       touchThreshold: 10,
@@ -19,27 +20,33 @@ function carouselsInit() {
     });
 
     $(el).on('beforeChange', (event, slick, currentSlide, nextSlide) => {
-      const nextHeight = $(`.js-carousel-slide[data-slick-index="${nextSlide}"]`).height();
-      $(el).parent('.js-accordion-nav-content').css('height', nextHeight);
+      const $nextSlide = $(`.js-carousel-slide[data-slick-index="${nextSlide}"]`);
+      const nextSlideHeight = $nextSlide.height();
+      $(el).parent('.js-accordion-nav-content').height(nextSlideHeight);
+      $nextSlide.find('.js-carousel-right-item').height(nextSlideHeight);
+      // console.log(nextSlideHeight);
     });
-  });
-}
 
-function setHeight() {
-  $('.js-carousel-slide').each((i, el) => {
-    const assetHeight = $(el).find('.js-carousel-asset').height();
-    const $detail = $(el).find('.js-accordion-nav-details');
-    const foo = assetHeight + 96;
-    $detail.css('min-height', foo);
+    function setHeight() {
+      if ($(el).closest('.js-accordion-nav-item').hasClass('active')) {
+        const currentSlideIndex = $(el).slick('slickCurrentSlide');
+        const $currentSlide = $(`.js-carousel-slide[data-slick-index="${currentSlideIndex}"]`);
+        const currentSlideHeight = $currentSlide.height();
+        $(el).parent('.js-accordion-nav-content').height(currentSlideHeight);
+        $currentSlide.find('.js-carousel-right-item').height(currentSlideHeight);
+        console.log(currentSlideHeight);
+      }
+    }
+
+    $(window).resize(setHeight);
   });
 }
 
 function init() {
   carouselsInit();
 
-  setHeight();
-  $(window).resize(setHeight);
-  $(window).on('orientationchange', setHeight);
+  // $(window).resize(setHeight);
+  // $(window).on('orientationchange', setHeight);
 }
 
 ee.addListener('init', init);
