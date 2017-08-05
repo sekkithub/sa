919d519e075a1e0ea13d56367089412e8de3d62d
Masato Seki <masatoseki@the-neighbourhood.com>
2017-08-05 15:58:10 +0100

    Mixcloud player style WIP

--- resources/scripts/components/mixcloud-player.js
+++ resources/scripts/components/mixcloud-player.js
@@ -1,24 +1,11 @@
 import $ from 'jquery';
 import ee from './ee';
 
-function setHeight() {
-  // $('.js-accordion-nav-item.active').each((i, el) => {
-  //   const iframWidth = $(el).find('.js-mixcloud-iframe').width() - 6;
-  //   $(el).find('.js-mixcloud-iframe').height(iframWidth);
-  // });
-}
-
-// function draw() {
-//   requestAnimationFrame(draw);
-//   setHeight();
-// }
-
 function init() {
-  // draw();
   const widget = Mixcloud.PlayerWidget(document.getElementById('foo'));
 
   widget.ready.then(() => {
-    $('.haha').on('click', () => {
+    $('.c-mixcloud-player__controller').on('click', () => {
       console.log('clicked');
       widget.play();
     });
