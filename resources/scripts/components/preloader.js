b1a26c712cfe7ccd17fb98615363e210b990fb0e
Masato Seki <masatoseki@the-neighbourhood.com>
2017-08-10 01:08:10 +0100

    Contact CMS driven

--- resources/scripts/components/preloader.js
+++ resources/scripts/components/preloader.js
@@ -2,23 +2,13 @@ import $ from 'jquery';
 import imagesLoaded from 'imagesloaded';
 import ee from './ee';
 
-const $html = $('html');
-const $body = $('body');
-
 function isLoaded() {
-  $body.addClass('isLoaded');
-  $html.removeClass('no-scroll');
+  $('html').removeClass('no-scroll');
+  $('body').addClass('isLoaded');
 }
 
-function hidePreloader() {
-  $body.removeClass('active-preloader');
-}
-
-export default function init() {
-  imagesLoaded($body, () => {
-    setTimeout(hidePreloader, 400);
-    setTimeout(isLoaded, 1000);
-  });
+function init() {
+  imagesLoaded('body', isLoaded);
 }
 
 ee.addListener('init', init);
