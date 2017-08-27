5046ad306976422bd71651a33e5eaa9e3a6919dd
Masato Seki <masatoseki@the-neighbourhood.com>
2017-08-27 22:22:17 +0100

    Layout simplified

--- resources/scripts/components/visuals.js
+++ resources/scripts/components/visuals.js
@@ -1,11 +1,7 @@
-import $ from 'jquery';
-// import slickLightbox from 'slick-lightbox';
+// import $ from 'jquery';
 import ee from './ee';
 
 function init() {
-  $('.c-visuals__content').slickLightbox({
-    itemSelector: '> .js-visuals-item',
-  });
 }
 
 ee.addListener('init', init);
