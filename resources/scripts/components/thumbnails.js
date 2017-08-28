dbad2461b572237c1e961db4ea89b670c818b568
Masato Seki <masatoseki@the-neighbourhood.com>
2017-08-28 18:25:34 +0100

    Removed infinite scroll

--- resources/scripts/components/thumbnails.js
+++ resources/scripts/components/thumbnails.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'slick-carousel';
 import ee from './ee';
 
-function indexingThumbnails() {
+export function indexingThumbnails() {
   $('.js-thumbnail-image').each((i, el) => {
     $(el).attr('data-index', i);
   });
